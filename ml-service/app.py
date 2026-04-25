b1a862eb96cd8aec1be99dd5e21b7a0d4b9771f1
kirti tiwari <kirtitiwari0312@gmail.com>
2026-04-25 22:38:06 +0530

    fix python version

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def predict_infection():
     })
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    #app.run(port=5001)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
